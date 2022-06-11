1e2b4d6deec8f56882919e398b1ffb3d4f3f2c7b
michal-romi <michal-romi@users.noreply.github.com>
2022-06-11 19:25:55 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d47ed818.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
